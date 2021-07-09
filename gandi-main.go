d0ab61152674c092a3733c806abae5147ba17949
PutuGandi <putugandi12@gmail.com>
2021-07-09 10:30:51 +0800

    Gandi App Say Hello

---
+++ gandi-main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gandi_say_hello "github.com/PutuGandi/gandi-go-say-hello"
+)
+
+func main() {
+	fmt.Println(gandi_say_hello.SayHello())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PutuGandi/gandi-app-say-hello
+
+go 1.15
+
+require github.com/PutuGandi/gandi-go-say-hello v1.2.0
