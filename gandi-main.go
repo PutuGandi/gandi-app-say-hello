2a31a507df69bf617b71bf0186c17b6e054c9463
PutuGandi <putugandi12@gmail.com>
2021-07-09 11:07:38 +0800

    using v2

--- gandi-main.go
+++ gandi-main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 
-	gandi_say_hello "github.com/PutuGandi/gandi-go-say-hello"
+	// gandi_say_hello "github.com/PutuGandi/gandi-go-say-hello"
+	gandi_say_hello "github.com/PutuGandi/gandi-go-say-hello/v2"
 )
 
+// func main() {
+// 	fmt.Println(gandi_say_hello.SayHello())
+// }
+
 func main() {
-	fmt.Println(gandi_say_hello.SayHello())
+	fmt.Println(gandi_say_hello.SayHello("Gandi Ganteng"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/PutuGandi/gandi-app-say-hello
 
 go 1.15
 
-require github.com/PutuGandi/gandi-go-say-hello v1.2.0
+require github.com/PutuGandi/gandi-go-say-hello/v2 v2.0.0
